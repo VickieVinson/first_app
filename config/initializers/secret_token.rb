b405fe3cbb01baf3117e169f4b8c9b18005133ee
VickieVinson <vickievinson@chartlogic.com>
2011-04-18 15:59:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '087da5baf61c81680aed4ded153f6696b159d7053e68c845c51a18c8a40f7e6b3410bf6bbefd3e6548c8cf3d47fa833977ec94021b73e2900d9fd51ac0bb3f0f'
